e4bd64daaabd8cc0ebeb04439526879227c15020
64309010012_Wuttipong <64309010012@tnk.ac.th>
2022-11-24 15:25:43 +0700

    Create ทดสอบ.js

---
+++ Week6/ทดสอบ.js
@@ -0,0 +1,73 @@
+ var obj =[{
+    
+    "Employee": "1001",
+    "Name": "นางสาว ชนิสา ไกรรอด",
+    "Department": "ไอที",
+    "Position": "System Engineer",
+    "Salary" : "42,000",
+    "Bonus": "S"
+},
+{
+    "Employee": "1002",
+    "Name": "นาย วศพล สิริอนันต์",
+    "Department": "ไอที",
+    "Position": "System Engineer",
+    "Salary" : "13,0000",
+    "Bonus": "A"
+},
+{
+    "Employee": "1003",
+    "Name": "นาง จิราพร อนทรประเสริฐ",
+    "Department": "ฝ่ายบุคคล",
+    "Position": "HR",
+    "Salary" : "9,000",
+    "Bonus": "C"
+},
+{
+    "Employee": "1004",
+    "Name": "นางสาว พิมพา กาญจนมาศ",
+    "Department": "ฝ่ายบุคคล",
+    "Position": "HR",
+    "Salary" : "21,000",
+    "Bonus": "B"
+},
+{
+    "Employee": "1005",
+    "Name": "นาย จิรโชติ วัฒนโกศล",
+    "Department": "การเงิน",
+    "Position": "Account",
+    "Salary" : "12,000",
+    "Bonus": "S"
+},
+{
+    "Employee": "1006",
+    "Name": "นาย ธัชพล ขจรเกียรติสกุล",
+    "Department": "การเงิน",
+    "Position": "Account",
+    "Salary" : "12,000",
+    "Bonus": "S"
+},
+{
+    "Employee": "1007",
+    "Name": "นาย ธนวิทย สุวรรณโชติ",
+    "Department": "การเงิน",
+    "Position": "Account",
+    "Salary" : "14,000",
+    "Bonus": "A"
+},
+{
+    "Employee": "1008",
+    "Name": "นางพัชรียา สําราญทรัพย",
+    "Department": "การเงิน",
+    "Position": "Account",
+    "Salary" : "50,000",
+    "Bonus": "C"
+},
+{
+    "Employee": "1009",
+    "Name": "นางสาว ขวัญชนก ธาดาวรวงศ",
+    "Department": "ไอที",
+    "Position": "System Engineer",
+    "Salary" : "16,000",
+    "Bonus": "A"
+},]
